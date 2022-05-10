9864faf41239c0d6bf7777b8571211d4d65cb479
larissecl <104574040+larissecl@users.noreply.github.com>
2022-05-10 11:34:13 -0300

    Add files via upload

---
+++ ContaEspecial.py
@@ -0,0 +1,14 @@
+from src.conta.conta import Conta
+
+class ContaEspecial(Conta):
+    def __init__(self, numero):
+        super().__init__(numero)
+        self.__bonus = 0
+
+    def renderBonus(self):
+        super().creditar(self.__bonus)
+        self.__bonus = 0
+
+    def creditar(self, valor):
+        self.__bonus = self.__bonus + (valor * 0.01)
+        super().creditar(valor)
